bea74b708939b2c2982c88f32d23cc6c853e3a9d
agungtrisutaji <agung.trisutaji.aprian@gmail.com>
2024-12-05 17:21:05 +0700

    change strcture

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+        'resources/js/countdown.js',
+        'resources/js/main.js',
+      ],
+      refresh: true,
+    }),
+  ],
 });
